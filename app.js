0eff6b02162da7df4907959ed1aa581893c4d761
shunto <s1610360@edu.cc.uec.ac.jp>
2020-10-24 23:56:55 +0900

    :tada: initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+$("#check-button-1").on('click', () => {
+    $(".list-1").toggleClass('on', 10000);
+    $("#check-button-1").html('^');
+});
+// $("#check-button-2").on('click', () => {
+//     $(".list-2").toggleClass('preview-list');
+// });
+// $("#check-button-3").on('click', () => {
+//     $(".list-3").toggleClass('preview-list');
+// });
+// $("#check-button-4").on('click', () => {
+//     $(".list-4").toggleClass('preview-list');
+// });
+// $("#check-button-5").on('click', () => {
+//     $(".list-5").toggleClass('preview-list');
+// });
