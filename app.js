0b6441896ccd5ed5d7fd3cc4f7df27f08097b4f2
shunto <s1610360@edu.cc.uec.ac.jp>
2020-11-29 19:55:23 +0900

    :tada: previewデザイン完成

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-$("#check-button-1").on('click', () => {
-    $(".list-1").toggleClass('on', 10000);
-    $("#check-button-1").html('^');
+// アロー関数だと失敗する
+$(".check-button").on('click', function() {
+    let title_body = ($(this).closest('.list-group-item').find('.preview-body'));
+    
+    if($(title_body).css("display") == "none") {
+        $(title_body).fadeIn("normal");
+    } else {
+        $(title_body).fadeOut("fast");
+    }
 });
-// $("#check-button-2").on('click', () => {
-//     $(".list-2").toggleClass('preview-list');
-// });
-// $("#check-button-3").on('click', () => {
-//     $(".list-3").toggleClass('preview-list');
-// });
-// $("#check-button-4").on('click', () => {
-//     $(".list-4").toggleClass('preview-list');
-// });
-// $("#check-button-5").on('click', () => {
-//     $(".list-5").toggleClass('preview-list');
-// });
